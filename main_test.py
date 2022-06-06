621d7bb26fd6fa7ed2d2cd0d39141e1f51297557
altafDevRev <i-altaf.ahmad@devrev.ai>
2022-06-06 16:48:58 +0530

    created new commit after noderunner

--- main_test.py
+++ main_test.py
@@ -6,6 +6,7 @@ def TestAdd():
         assert Add(8,6) == 14
         assert Add(10,10) == 20
         print("Add Function works correctly")
+        print("Add Function also works")
 
 if __name__ == '__main__':
         TestAdd()
