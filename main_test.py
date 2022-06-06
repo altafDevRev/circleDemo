f498c2bf949c53666d9f519851d7a9bcd1857723
altafDevRev <i-altaf.ahmad@devrev.ai>
2022-06-06 12:45:35 +0530

    resolved tests again

--- main_test.py
+++ main_test.py
@@ -2,7 +2,8 @@
 from main import Add
 
 def TestAdd():
-        assert Add(2,3) == 6
+        assert Add(2,3) == 5
+        assert Add(8,6) == 14
         print("Add Function works correctly")
 
 if __name__ == '__main__':
